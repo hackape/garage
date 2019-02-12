3aa66a3a34bf4367b43d29e7f9a79d9c5c6fb3a5
hackape <vince@hackape.com>
2019-02-12 18:25:14 +0800

    Create findReact.js

---
+++ findReact.js
@@ -0,0 +1,19 @@
+window.FindReact = function(dom) {
+    var internalInstance = null;
+    for (var key in node) {
+        if (key.startsWith("__reactInternalInstance$")) {
+            internalInstance = node[key];
+            break;
+        }
+    }
+    
+    if (!internalInstance) return null;
+
+    if (internalInstance.return) { // react 16+
+        return internalInstance._debugOwner
+            ? internalInstance._debugOwner.stateNode
+            : internalInstance.return.stateNode;
+    } else { // react <16
+        return internalInstance._currentElement._owner._instance;
+    }
+}
